eba7bf041914bcecd3638823dca9c30fe3b2023d
Tim Swan <tswan@atlassian.com>
2018-01-11 16:02:12 +1100

    Removing a line of code completely to get a change for a comment on a commit

--- index.php
+++ index.php
@@ -2,17 +2,8 @@
 
 define('DS', DIRECTORY_SEPARATOR);
 
-// load kirby
 
-require(__DIR__ . DS . 'kirby' . DS . 'bootstrap.php');
 
-// check for a custom site.php
-
-if(file_exists(__DIR__ . DS . 'site.php')) {
-  require(__DIR__ . DS . 'site.php');
-} else {
-  $kirby = kirby();
-}
 
 // render
 
