568f3dbc72cebb8bb466bab954a1fbfb9a42fec5
Tim Swan <tswan@atlassian.com>
2018-01-11 15:50:00 +1100

    Scenario 2 and 5

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 
 define('DS', DIRECTORY_SEPARATOR);
 
+// load kirby
+
+require(__DIR__ . DS . 'kirby' . DS . 'bootstrap.php');
+
 // check for a custom site.php
+
 if(file_exists(__DIR__ . DS . 'site.php')) {
   require(__DIR__ . DS . 'site.php');
 } else {
